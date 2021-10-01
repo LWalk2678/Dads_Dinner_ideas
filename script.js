7698fe0f30c062b2ce8c12d85c0bcf80561403fb
Lance Walker <LWalk2678@msn.com>
2021-10-01 15:03:22 -0600

    working on API

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=$
-const API_KEY = '31234ea2'
+
+
+// const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?&${input}&apiKey=${API_KEY}`
+const API_KEY = '43b709665eea4cd3ab3f107fb001007f'
+
+// https://api.spoonacular.com/recipes/complexSearch?&pasta&apiKey=43b709665eea4cd3ab3f107fb001007f
+
+button.addEventListener(('click'), () => {
+  const input = document.getElementById('blank').value;
+  // const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${input}`;
+  const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?&${input}&apiKey=${API_KEY}`
+  // const IMG_URL = `http://img.omdbapi.com/?apikey=${API_KEY}&s=${input}`
+
+  fetch(BASE_URL)
+    .then((results) => {
+      return results.json();
+    })
+    .then((resultsJson) => {
+      console.log(resultsJson);
+      renderList(resultsJson.Search)
+    })
+    .catch((error) => {
+      console.log(`ERROR: ${error}`);
+    })
+})
