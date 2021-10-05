657af9dd09a9cfc73f2cbf2c8350522dd876c32f
Lance Walker <LWalk2678@msn.com>
2021-10-05 13:59:24 -0600

    .

--- script.js
+++ script.js
@@ -2,37 +2,60 @@
 
 // const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?&${input}&apiKey=${API_KEY}`
 
+//==========DropDown Menu===================
 
-
-const searchButton = document.querySelector('button');
-
-searchButton.addEventListener('click', () => {
-  console.log("search button clicked");
+(function () {
+  var main = document.getElementById('mainIngredient');
+  var beefMeals = {
+    meal1: 1063645,
+    meal2: 636167,
+    meal3: 10000566,
+  };
+  var chickenMeals = {
+    meal1: 1063645,
+    meal2: 636167,
+    meal3: 10000566,
+  };
+  
+  function pullInfo(selected) {
   const input = document.getElementById('blank').value;
 
   const API_KEY = '43b709665eea4cd3ab3f107fb001007f';
 
-  const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?&${input}&apiKey=${API_KEY}`;
+  const Id_URL = `https://api.spoonacular.com/recipes/${idNum}/information?apiKey=${API_KEY}`;
 
-  const Ing_URL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${input}&number=20&apiKey=${API_KEY}`;
+  console.log("dropdown fuction running");
 
-  console.log(Ing_URL);
 
-  fetch(Ing_URL)
+  fetch(Id_URL)
     .then((results) => {
       return results.json();
     })
     .then((resultsJson) => {
       console.log(resultsJson);
-      renderList(resultsJson)
+      renderList1(resultsJson)
     })
     // .catch((error) => {
     //   console.log(`ERROR: ${error}`);
     // })
-})
-console.log("hi");
+}
+  
 
-const renderList = (mealList) => {
+})
+
+
+
+
+function getMainIngredient(choice) {
+  if (choice === 'Beef') {
+    return 1063645;
+  } else if (choice === 'Chicken') {
+    return 715525;
+  }
+
+}
+
+const renderList1 = (mealList) => {
   document.querySelector('.meal-list').innerHTML = '';
   mealList.forEach(meal => {      
 
@@ -54,3 +77,58 @@ const renderList = (mealList) => {
     document.querySelector('.meal-list').append(mealData);
   })
 }
+
+
+
+//=========Search by Ingerdient Section===========
+const searchButton = document.querySelector('button');
+
+searchButton.addEventListener('click', () => {
+  console.log("search button clicked");
+  const input = document.getElementById('blank').value;
+
+  const API_KEY = '43b709665eea4cd3ab3f107fb001007f';
+
+  const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?&${input}&apiKey=${API_KEY}`;
+
+  const Ing_URL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${input}&number=4&apiKey=${API_KEY}`;
+
+  console.log("Search function running");
+
+  fetch(Ing_URL)
+    .then((results) => {
+      return results.json();
+    })
+    .then((resultsJson) => {
+      console.log(resultsJson);
+      renderList2(resultsJson)
+    })
+    // .catch((error) => {
+    //   console.log(`ERROR: ${error}`);
+    // })
+})
+console.log("hi");
+
+const renderList2 = (mealList) => {
+  document.querySelector('.meal-list').innerHTML = '';
+  mealList.forEach(meal => {      
+
+    const mealData = document.createElement('div');
+    mealData.classname = 'mealData'
+
+    const mealTitle = document.createElement('h4')
+      mealTitle.innerText = meal.title
+      mealData.append(mealTitle)
+
+    const mealID = document.createElement('h5')
+      mealID.innerText = meal.id
+      mealData.append(mealID)
+
+    const mealPic = document.createElement('img')
+      mealPic.src = meal.image
+      mealData.append(mealPic)
+
+    document.querySelector('.meal-list').append(mealData);
+  })
+}
+
