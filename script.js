f157d430c72e5e155d13b100be91ea2ea85fe003
Lance Walker <LWalk2678@msn.com>
2021-10-08 07:11:45 -0600

    cleaned code lines

--- script.js
+++ script.js
@@ -7,30 +7,26 @@ const dropButton = document.querySelector("input");
 let main = document.getElementById("mainIngredient");
 
 const dropResults = (easyMeal) => {
-  console.log("Dropdown button clicked");
   idNum = main.value;
 
   const API_KEY = "43b709665eea4cd3ab3f107fb001007f";
   const Id_URL = `https://api.spoonacular.com/recipes/${idNum}/information?apiKey=${API_KEY}`;
-  console.log("dropdown function running");
 
   fetch(Id_URL)
     .then((results) => {
       return results.json();
     })
     .then((resultsJson) => {
-      console.log(resultsJson);
       renderList1(resultsJson);
     })
     .catch((error) => {
-    console.log(`ERROR: ${error}`);
-  })
+      console.log(`ERROR: ${error}`);
+    });
 };
-console.log("dropdown function done");
 
 const renderList1 = (mealList) => {
   document.querySelector(".meal-list").innerHTML = "";
-  
+
   const mealData = document.createElement("div");
   mealData.classname = "mealData";
 
@@ -45,7 +41,7 @@ const renderList1 = (mealList) => {
 
   const mealInst = document.createElement("p");
   mealInst.innerText = mealList.instructions;
-  mealInst.className = "Instructions";
+  mealInst.className = "instructions";
   mealData.append(mealInst);
 
   document.querySelector(".meal-list").append(mealData);
@@ -55,7 +51,6 @@ const renderList1 = (mealList) => {
 const searchButton = document.querySelector("button");
 
 searchButton.addEventListener("click", () => {
-  console.log("search button clicked");
   const input = document.getElementById("blank").value;
 
   const API_KEY = "43b709665eea4cd3ab3f107fb001007f";
@@ -64,21 +59,17 @@ searchButton.addEventListener("click", () => {
 
   const Ing_URL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${input}&number=6&apiKey=${API_KEY}`;
 
-  console.log("Search function running");
-
   fetch(Ing_URL)
     .then((results) => {
       return results.json();
     })
     .then((resultsJson) => {
-      console.log(resultsJson);
       renderList2(resultsJson);
     })
     .catch((error) => {
-    console.log(`ERROR: ${error}`);
-  })
+      console.log(`ERROR: ${error}`);
+    });
 });
-console.log("hi");
 
 const renderList2 = (mealList) => {
   document.querySelector(".meal-list").innerHTML = "";
@@ -92,7 +83,7 @@ const renderList2 = (mealList) => {
 
     const mealPic = document.createElement("img");
     mealPic.src = meal.image;
-    mealPic.id = meal.id
+    mealPic.id = meal.id;
     mealPic.className = "mealPicture";
     mealData.append(mealPic);
 
@@ -104,5 +95,3 @@ const renderList2 = (mealList) => {
     document.querySelector(".meal-list").append(mealData);
   });
 };
-
-
